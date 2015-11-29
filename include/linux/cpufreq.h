52f9b6acb0a2e79844491f673ceb550491f58881
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-11-29 17:48:11 -0600

    Add XPerience Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -385,6 +385,9 @@ extern struct cpufreq_governor cpufreq_gov_bioshock;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lionheart)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_XPERIENCE)
+extern struct cpufreq_governor cpufreq_gov_xperience;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_xperience)
 #endif
 
 
