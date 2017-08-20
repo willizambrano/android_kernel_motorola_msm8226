b26337b001517fb49a50bc1d434313c7f5d2fc6f
willizambrano <zambranowilliam21@gmail.com>
2017-08-20 10:50:37 -0400

    Start OC 1593mhz

--- build.sh
+++ build.sh
@@ -14,11 +14,9 @@ echo "                 Borrando basura         "
 echo -e "*****************************************************$nocol"
 
 rm -rf arch/arm/boot/*.dtb
-rm -f /home/william/android_kernel_motorola_msm8226/arch/arm/boot/zImage-dtb
-rm -f /home/william/android_kernel_motorola_msm8226/arch/arm/boot/zImage
 make clean && make mrproper
 
-export CROSS_COMPILE=/home/william/linaro4.9/bin/arm-cortex_a7-linux-gnueabihf-
+export CROSS_COMPILE=/home/william/linaro4.9/bin/arm-eabi-
 export ARCH=arm
 export KBUILD_BUILD_USER="WilliamZambrano"
 export KBUILD_BUILD_HOST="AMDfx6300"
@@ -30,8 +28,6 @@ echo "           Compilando Evolution_Kernel         "
 echo -e "*****************************************************$nocol"
 
 make -o3 -j6 CONFIG_DEBUG_SECTION_MISMATCH=y CONFIG_NO_ERROR_ON_MISMATCH=y
-
-mv /home/william/android_kernel_motorola_msm8226/arch/arm/boot/zImage-dtb /home/william/android_kernel_motorola_msm8226/Evolution_Kernel/boot
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
