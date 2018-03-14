70febe74280dfb323095b22c8eb7976750380761
willizambrano <zambranowilliam21@gmail.com>
2018-03-14 14:31:08 -0400

    NoDynamic FSync

--- build.sh
+++ build.sh
@@ -16,18 +16,19 @@ echo -e "*****************************************************$nocol"
 rm -rf arch/arm/boot/*.dtb
 make clean && make mrproper
 
-export CROSS_COMPILE=/home/joker/linaro4.9/bin/arm-cortex_a7-linux-gnueabihf-
+export CROSS_COMPILE=/home/william/linaro5.2/bin/arm-eabi-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER="WilliamZambrano"
 export KBUILD_BUILD_HOST="OreoRules"
 
 make falcon_defconfig
+make menuconfig
 echo -e "$blue*****************************************************"
 echo "           Compilando Evolution_Kernel         "
 echo -e "*****************************************************$nocol"
 
-make -o3 -j6 CONFIG_DEBUG_SECTION_MISMATCH=y CONFIG_NO_ERROR_ON_MISMATCH=y
+make -o3 -j6
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
