c749a6ccb0263e8fc2fb404b05f2fecde975c7e6
willizambrano <zambranowilliam21@gmail.com>
2018-07-15 02:07:15 -0400

    zram: Backport from Linux 4.1 and promote from staging

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -52,10 +52,11 @@ struct msm_thermal_data {
 	int32_t ocr_temp_degC;
 	int32_t ocr_temp_hyst_degC;
 };
+#endif
 
 #if defined(CONFIG_THERMAL_MONITOR) ||\
-      defined(CONFIG_INTELLI_THERMAL) ||\
-      defined(CONFIG_INTELLI_THERMAL_V2)
+    defined(CONFIG_INTELLI_THERMAL) ||\
+    defined(CONFIG_INTELLI_THERMAL_V2)
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
 extern int msm_thermal_device_init(void);
 extern int msm_thermal_set_frequency(uint32_t cpu, uint32_t freq,
